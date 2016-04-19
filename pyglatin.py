cc1b8655f8d8175dbd978514a586358029cb1592
Ian <surouxx@gmail.com>
2016-04-19 13:08:25 -0400

    Pyg Latin Translator

--- pyglatin.py
+++ pyglatin.py
@@ -2,11 +2,11 @@ pyg = 'ay'
 
 original = raw_input('Enter a word:')
 
-if len(original) > 0 and original.isalpha():
-    print original
+if len(original) < 0 and original.isalpha():
+    print '%s try again' % original
 else:
-    print 'empty'
-word = original.lower()
-first = word[0]
-new_word = word + first + pyg
-new_word = word[1:] + first + pyg
+    word = original.lower()
+    first = word[0]
+    new_word = word + first + pyg
+    new_word = word[1:] + first + pyg
+    print '%s'% new_word
